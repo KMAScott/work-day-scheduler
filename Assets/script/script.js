4a88a9d6b1c53bcf05379154dccb98ff59e0caca
Scott Ashley <burnbright987@gmail.com>
2022-05-04 13:43:53 -0400

    added save and recall functionality, colorcoding

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -1,2 +1,43 @@
-var currentTime = dayjs().format('dddd, M-D-YYYY h:mm A');
+var currentTime = dayjs().format('dddd, MMMM D, YYYY h:mm A');
 $("#currentDay").html(currentTime);
+
+$(document).ready(function() {
+    $(".saveBtn").on("click", function() {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, text);
+    })
+
+    function timeCheck() {
+        var currentHour = dayjs().hour();
+        $(".time-block").each(function() {
+            var rowTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            if (rowTime < currentHour) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            } else if (rowTime === currentHour) {
+                $(this).removeClass("future");
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            } else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour1 .description").val(localStorage.getItem("hour1"));
+    $("#hour2 .description").val(localStorage.getItem("hour2"));
+    $("#hour3 .description").val(localStorage.getItem("hour3"));
+    $("#hour4 .description").val(localStorage.getItem("hour4"));
+    $("#hour5 .description").val(localStorage.getItem("hour5"));
+
+    timeCheck();
+})
